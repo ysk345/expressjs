c5f9a144121bfdd8e5fb31c59c6acb46bc4743a1
ysk345 <yskrenee@gmail.com>
2023-09-28 18:11:42 -0400

    minor updates

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 process.env.NODE_DEV = process.env.NODE_DEV || 'development';
 const express = require('./config/express');
 const app = express();
 app.listen(port);
 module.exports = app;
 
-console.log('Express is running on port 3000');
+console.log('Express is running on port 8080');
