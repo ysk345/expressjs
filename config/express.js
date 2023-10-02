ba149a798cf56c3dbf9373a0683dab09fe597c15
ysk345 <yskrenee@gmail.com>
2023-10-01 23:25:43 -0400

    layout and style updates

--- config/express.js
+++ config/express.js
@@ -17,11 +17,12 @@ module.exports = () => {
     app.use(bodyParser.urlencoded({ //only parse body when url is encoded
         extended: true
     }));
+    
     app.use(bodyParser.json());
     app.use(methodOverride());
     app.set('views', 'views'); //folder name with ejs templates
     app.set('view engine', 'ejs'); //use ejs view engine
-    app.use('/', require('../routes/index.server.routes.js'));
+    app.use('/', require('../routes/index.server.routes.js')); //path to routes
     app.use('/public/img', express.static('./public/img'));
     app.use(express.static("./node_modules"));
 
